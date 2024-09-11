e0bb63d4a91dc8f8d66a24d4b098a8b6e6649889
ashco <ashco@Joash.dhcp.wustl.edu>
2024-09-11 15:35:01 -0500

    Draft 1

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,39 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		//start amount 
+		System.out.println("Start amount of money: ");
+		int startAmount = in.nextInt();
+		
+		//Win Chance
+		System.out.println("Win Chance: ");
+		double winChance = in.nextDouble();
+		
+		//Win limit
+		System.out.println("Win Limit: ");
+		int winLimit = in.nextInt(); 
+		
+		//loop
+		
+		while (startAmount > 0 && startAmount < winLimit) {
+			if ( Math.random()< winChance) {
+				int newAmount = startAmount + 1;
+				System.out.println(newAmount);
+				
+			}
+			else {
+				int newAmount = startAmount - 1;
+				System.out.println(newAmount);
+			}
+
+			}
+		
+	
+	}
+
+}
